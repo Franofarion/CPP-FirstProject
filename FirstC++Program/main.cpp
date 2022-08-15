885e6858448ac89c84049cf032a0729265bb7eb6
Baptiste Domange <baptiste.domange@gmail.com>
2022-08-15 18:10:48 +0200

    use of cout & cin

--- FirstC++Program/main.cpp
+++ FirstC++Program/main.cpp
@@ -2,13 +2,19 @@
 //  main.cpp
 //  FirstC++Program
 //
-//  Created by Baptiste  Domange on 15/08/2022.
+//  Created by Baptiste Domange on 15/08/2022.
 //
 
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "What is your name?" << endl;
+    
+    string name {};
+    cin >> name;
+    cout << "You said your name is: " << name << endl;
     return 0;
 }
