72a9be8e62c70911a216a2d5ec8a69c5419b0f64
Baptiste Domange <baptiste.domange@gmail.com>
2022-08-15 17:58:16 +0200

    Initial Commit

---
+++ FirstC++Program/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FirstC++Program
+//
+//  Created by Baptiste  Domange on 15/08/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
